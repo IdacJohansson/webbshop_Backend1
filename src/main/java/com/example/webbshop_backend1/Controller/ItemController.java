b33278492252e4e50bc0bf7849e70cf23d447ecf
Staz1x <fogel.stazix@gmail.com>
2023-04-26 13:38:54 +0200

    PostMapping started

--- src/main/java/com/example/webbshop_backend1/Controller/ItemController.java
+++ src/main/java/com/example/webbshop_backend1/Controller/ItemController.java
@@ -2,6 +2,8 @@ package com.example.webbshop_backend1.Controller;
 
 import com.example.webbshop_backend1.Model.Items;
 import com.example.webbshop_backend1.Repo.ItemsRepo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +27,13 @@ public class ItemController {
     @RequestMapping("items")
     public List<Items> getAllItems(){
         return itemsRepo.findAll();
+    }
 
+    //curl http://localhost:8080/items/add -H "Content-Type:application/json" -d "{\"name\":\"Green beret\", \"price\":\"324\"}" -v
+    @PostMapping("items/add")
+    public List<Items> addItems(@RequestBody Items i) {
+        itemsRepo.save(i);
+        return itemsRepo.findAll();
     }
 }
 
